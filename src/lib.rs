485402e7ae9e2572d73a3a59bfa478872ccfd98f
lassade <lassade@localhost.localdomain>
2020-07-15 13:28:51 -0300

    fixed encoding error

--- src/lib.rs
+++ src/lib.rs
@@ -97,10 +97,10 @@ const HEX_ENCODE: [u8; 512] =
 
 pub fn encode(input: &[u8]) -> String {
     let mut i = 0usize;
-    let mut v = alloc(input.len() << 2);
+    let mut v = alloc(input.len() << 1);
     unsafe { 
         for b in input {
-            let j = (*b as usize) << 2;
+            let j = (*b as usize) << 1;
             *v.get_unchecked_mut(i) = *HEX_ENCODE.get_unchecked(j);
             *v.get_unchecked_mut(i | 1) = *HEX_ENCODE.get_unchecked(j | 1);
             i += 2;
