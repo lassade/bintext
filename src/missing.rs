196adcf9a5a2d8ebdb49af7cbbbb226d2358081a
lassade <lassade@localhost.localdomain>
2020-08-17 02:37:12 -0300

    intrinsic test

--- src/missing.rs
+++ src/missing.rs
@@ -31,3 +31,40 @@ pub unsafe fn _mm_storeu_si64(mem_addr: *mut i8, a: __m128i) {
     *mem_addr.add(6) = *v.get_unchecked(6);
     *mem_addr.add(7) = *v.get_unchecked(7);
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    #[test]
+    fn intrinsic_mm_set_epi64x() {
+        unsafe { 
+            let mut vals = [0x0Fu8; 20];
+            let a = _mm_set_epi64x(0, 0x07_06_05_04_03_02_01_00);
+
+            let mut ofs = 0;
+            let mut p = vals.as_mut_ptr() as *mut i8;
+
+            // Make sure p is **not** aligned to 16-byte boundary
+            if p.align_offset(16) == 0 {
+                ofs = 1;
+                p = p.offset(1);
+            }
+
+            super::_mm_storeu_si64(p, a);
+
+            if ofs > 0 {
+                assert_eq!(vals[ofs - 1], 0x0F);
+            }
+            for i in 0..8 {
+                assert_eq!(vals[ofs + i], i as u8);
+            }
+            assert_eq!(vals[ofs + 8], 0x0F);
+            assert_eq!(vals[19], 0x0F);
+        }
+    }
+}
